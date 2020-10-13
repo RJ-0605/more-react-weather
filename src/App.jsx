42788e86a4707a583b7e96e3dbc74dd0a3c9842d
rj-0605 <rojedkay@gmail.com>
2020-10-13 20:40:41 +0000

    work done in half left with rendering on page and saving displaying current on ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React ,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import LoginComponent from './component/LoginComponent';
@@ -17,115 +17,103 @@ import './weather.css';
 import weatherjson from './weatherdata.json'
 
 
-
+                                          
 
 function App() {
 
 
-          // <input   placeholder="Search.."  style={{float: 'right', padding: '6px', border: 'none', marginTop: '15px', marginRight: '16px',fontSize: '17px',}} />
+  // <input   placeholder="Search.."  style={{float: 'right', padding: '6px', border: 'none', marginTop: '15px', marginRight: '16px',fontSize: '17px',}} />
 
-    const [email, setEmail]=useState("");
-    const [username, setUsername]=useState("");
-    const [password, setPassword]=useState("");
-    const [loggedin, setLoggedIn]=useState(false);
-    const [apireached, setApireached]=useState(false);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedin, setLoggedIn] = useState(false);
+  const [apireached, setApireached] = useState(false);
 
-    const [isPageLoading, setisPageLoading ] = useState(true);
+  const [isPageLoading, setisPageLoading] = useState(true);
 
-    const [count, setCount ] = useState(0)
-
-
-    const[firstarr,setFirstArr]=useState({
-
-                                    user:{
-                                      id:12,
-                                      firstName:"John",
-                                      lastName:"Doe",
-                                    },
-               }
-               )
+  const [count, setCount] = useState(0)
+  const [searchHistory, setSearchHistory] = useState([])
 
 
-    useEffect(()=>{
+  const [firstarr, setFirstArr] = useState({
 
-      axios.get('http://api.weatherstack.com/current?access_key=7e9be14a3120f70d8fd052c7141cdfbc&query=New York')
-         .then(res=> {
-          console.log(res.data);
-          
-          setApireached(true)
-          setisPageLoading(false)
-                 })
-         .catch( err => console.error(err) );
-         console.log(firstarr.user.id) ;
-         
+    user: {
+      id: 12,
+      firstName: "John",
+      lastName: "Doe",
+    },
+  }
+  )
 
-             }    
+  const saveSearchHistory = (search) => {
+    // Check If User If Logged In
+    if (loggedin) {
+      // Get Existing Data
+      const existingSearchesJSON = localStorage.getItem('searchHistory');
+      const existingSearches = existingSearchesJSON ? JSON.parse(existingSearchesJSON) : [];
+      console.log(existingSearches);
+      // Add New Data
+      existingSearches.push(search);
+      console.log(existingSearches);
+      // Push Data to localStorage
+      localStorage.setItem('searchHistory', JSON.stringify(existingSearches));
+      // Set Saved History To State
+      setSearchHistory(existingSearches);
+    }
+  }
 
-      );
+
+  useEffect(() => {
+
+    axios.get('http://api.weatherstack.com/current?access_key=7e9be14a3120f70d8fd052c7141cdfbc&query=New York')
+      .then(res => {
+        console.log(res.data);
+        saveSearchHistory(res.data);
+
+        setApireached(true)
+        setisPageLoading(false)
+      })
+      .catch(err => console.error(err));
+    console.log(firstarr.user.id);
 
 
+  }, []);
 
-
-
-
-
-    
-
-     console.log('this text is reqpa')
-
-
+  console.log('this text is reqpa');
 
 
   return (
-    
-
     <div >
+      <div>
+        <br />
+        <form className="col-md-12" >
+          <div className="input-group">
+            <input type="text" className="form-control" id="search" style={{ height: 56 }} placeholder="Search for..." />
+            <span className="input-group-btn">
+              <button className="btn btn-default bg-light" type="submit" style={{ height: 56 }}>Go!</button>
+            </span>
+          </div>
+        </form>
+        <br />
+        <br />
+        <br />
 
-    {
-    isPageLoading === true
-      ?
-    <p   style={{height: 56, color:"red" , width:200 ,margin: "auto" , marginTop:30,}}  > This page is Loading </p>
+        <LeftDisplayComponent />
 
-    :
+        { /*if else will be here  to load component  logged in after logged in  */}
 
-    <div>
+        <LoginComponent email={email} setEmail={setEmail} username={username} setUsername={setUsername} password={password} setPassword={setPassword} loggedin={loggedin} setLoggedIn={setLoggedIn} />
 
-    <br />
-          <form className="col-md-12" >
-                    <div className="input-group">
-                        <input type="text" className="form-control" id="search"  style={{height: 56}} placeholder="Search for..."/>
-                        <span className="input-group-btn">
-                            <button className="btn btn-default bg-light" type="submit" style={{height: 56}}>Go!</button>
-                        </span>
-                    </div>
-                </form>
-       <br />
-       <br />
-       <br />
-      
-      <LeftDisplayComponent />
-
-      { /*if else will be here  to load component  logged in after logged in  */}
-
-      <LoginComponent   email={email} setEmail={setEmail} username={username} setUsername={setUsername}  password={password} setPassword={setPassword} loggedin={loggedin} setLoggedIn={setLoggedIn}  />
-      
-        
-      
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
-      
-      }
-     
-       
     </div>
-      
+
   );
 }
 
